7d3bf495b4ba88b19ab7702704a5938849576ba9
Mehmet Kuneroğlu <mehmetkuneroglu@gmail.com>
2023-11-10 23:29:54 +0300

    Proje dosyası ekle.

---
+++ TeknikServis_Web/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TeknikServis_Web.Entity;
+
+namespace TeknikServis_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMIZDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = (from x in db.TBLURUN
+                                    select new
+                                    {
+                                        x.ID,
+                                        x.AD,
+                                        x.MARKA,
+                                        x.SATISFIYAT
+                                    }).ToList();
+            Repeater2.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TBLILETISIM t = new TBLILETISIM();
+            t.ADSOYAD = TextBox1.Text;
+            t.EMAIL = TextBox2.Text;
+            t.KONU = TextBox3.Text;
+            t.MESAJ = TextBox4.Text;
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+            Label1.Text = "Mesajınız gönderildi";
+            TextBox1.Text = null;
+            TextBox2.Text = null;
+            TextBox3.Text = null;
+            TextBox4.Text = null;
+
+
+        }
+    }
+}
